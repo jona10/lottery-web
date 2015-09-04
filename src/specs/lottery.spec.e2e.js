5d4319b36bfc248bc821b8e51fd701740e20f80a
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2015-09-04 15:36:00 -0400

    We'd like to buy a ticket.

--- src/specs/lottery.spec.e2e.js
+++ src/specs/lottery.spec.e2e.js
@@ -7,4 +7,16 @@ describe("The application", function () {
             expect(text).to.equal('Lotterie Pyxis', 'the page header');
         });
     });
+
+    it.skip("should display the winner's name and their prize after the draw", function(){
+        browser.url('http://localhost:3000/');
+        browser.setValue('input#participant-name', 'Audrée');
+        browser.click('button#buy-ticket');
+        browser.getText('li.participant .name').then(function(text){
+            expect(text).to.equal('Audrée', 'the participant\'s name');
+        });
+        browser.getText('li.participant .ticket-number').then(function(text){
+            expect(text).to.be.within(1, 3, 'the ticket\'s number');
+        });
+    });
 });
